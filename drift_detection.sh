70b2eca30642e4485dad47f0d58b08bda3b5ed12
Carlos Bruno E <cbruno@spsolutions.com.mx>
2022-01-17 16:03:27 -0600

    Se agregan comandos de Drift Detection a script

---
+++ drift_detection.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Se ejecuta AWS para detectar la desviación/drift del stack. Este genera un ID.
+DRIFT=$(aws cloudformation detect-stack-drift --stack-name "${{env.fullStackName}}" | jq '.StackDriftDetectionId')
+
+# Se le quitan las comillas dobles a la variable de ambiente.
+DRIFT=$(echo "${DRIFT//'"'}")
+
+echo "El id de la deteccion del drift es:" $DRIFT
+
+# A veces la detección del drift es larga, por lo cual podemos verificar 
+# el estatus basado en su ID con el comando siguiente:
+
+DETECTIONSTATUS=$(aws cloudformation describe-stack-drift-detection-status --stack-drift-detection-id ${DRIFT} | jq '.DetectionStatus')
+
+# Se le quitan las comillas dobles a la variable de ambiente DETECTIONSTATUS
+DETECTIONSTATUS=$(echo "${DETECTIONSTATUS//'"'}")
+
+# El siguiente bucle verifica cada 2 segundos el estatus de la detección e imprime el estado actual. 
+# Y no brinca al siguiente paso hasta que tenga un estado de: detección completada.
+while [[ $DETECTIONSTATUS != "DETECTION_COMPLETE" || $DETECTIONSTATUS != "DETECTION_FAILED" ]]; do sleep 2; echo $DETECTIONSTATUS; \
+DETECTIONSTATUS=$(aws cloudformation describe-stack-drift-detection-status --stack-drift-detection-id ${DRIFT} | jq '.DetectionStatus'); \
+DETECTIONSTATUS=$(echo "${DETECTIONSTATUS//'"'}"); done
+echo $DETECTIONSTATUS
+
+# Se muestra el estado actual del drift del stack 
+DRIFTSTATUS=$(aws cloudformation describe-stack-drift-detection-status --stack-drift-detection-id $DRIFT | jq '.StackDriftStatus')
+DRIFTSTATUS=$(echo "${DRIFTSTATUS//'"'}")
+echo $DRIFTSTATUS
+
+
+# Dependiendo del estado de drift actual se imprimen los mensajes correspondientes.
+if [[ $DRIFTSTATUS = "IN_SYNC" ]]; then MENSAJEDRIFT=$"no tiene DRIFTS."; \
+elif [[ $DRIFTSTATUS = "DRIFTED" ]]; then MENSAJEDRIFT=$"tiene modificaciones manuales. Para mayor información visitar directamente Cloudformation."; \
+elif [[ $DRIFTSTATUS = "DETECTION_FAILED" ]]; then MENSAJEDRIFT=$"verifica que el usuario de GitHub tenga permisos suficientes para los recursos del Stack."; \
+elif [[ $DRIFTSTATUS = "NOT_CHECKED" ]]; then echo "aún no tiene listo el estatus. Verificar en CloudFormation."; fi
+STACKS=$"El Stack"
+
+curl -H 'Content-Type: application/json' \
+          --retry 4 \
+          --retry-delay 1 \
+          -d '{"text": "'"$DRIFTSTATUS"' - '"$STACKS"' - '"$PREFIX-$STACK_NAME"' - '"$MENSAJEDRIFT"'"}' \
+          https://mxspsolutions.webhook.office.com/webhookb2/a84b113f-9d27-4138-84ae-e3e4ab4830f2@ac5349df-152e-486f-9b39-fe3c4a25efe0/IncomingWebhook/e2f3db57ac59459a938f44c822548b89/10ed5eda-9b70-4598-858a-e5ae6599fa66
