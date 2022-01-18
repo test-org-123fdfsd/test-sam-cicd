cbec7e6b2b48898678d21d0ffea2ed46a8d0787e
Carlos Bruno E <cbruno@spsolutions.com.mx>
2022-01-18 14:01:05 -0600

    Se declara STACKNAME con valor de nombre del stack en script

--- drift_detection.sh
+++ drift_detection.sh
@@ -34,11 +34,11 @@ if [[ $DRIFTSTATUS = "DRIFTED" ]]; then MENSAJEDRIFT=$"tiene modificaciones manu
 elif [[ $DRIFTSTATUS = "DETECTION_FAILED" ]]; then MENSAJEDRIFT=$"verifica que el usuario de GitHub tenga permisos suficientes para los recursos del Stack."; \
 elif [[ $DRIFTSTATUS = "NOT_CHECKED" ]]; then echo "aún no tiene listo el estatus. Verificar en CloudFormation."; fi
 STACKS=$"El Stack"
-
+STACKNAME=(${{env.fullStackName}})
 if [[ $DRIFTSTATUS = "DRIFTED" || $DRIFTSTATUS = "DETECTION_FAILED" || $DRIFTSTATUS = "NOT_CHECKED" ]]; then \
 curl -H 'Content-Type: application/json' \
           --retry 4 \
           --retry-delay 1 \
-          -d '{"text": "'"$DRIFTSTATUS"' - '"$STACKS"' - '"$PREFIX-$STACK_NAME"' - '"$MENSAJEDRIFT"'"}' \
+          -d '{"text": "'"$DRIFTSTATUS"' - '"$STACKS"' - '"$STACK_NAME"' - '"$MENSAJEDRIFT"'"}' \
           https://mxspsolutions.webhook.office.com/webhookb2/a84b113f-9d27-4138-84ae-e3e4ab4830f2@ac5349df-152e-486f-9b39-fe3c4a25efe0/IncomingWebhook/e2f3db57ac59459a938f44c822548b89/10ed5eda-9b70-4598-858a-e5ae6599fa66; \
           fi
