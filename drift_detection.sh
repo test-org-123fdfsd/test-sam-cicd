4dbe02a0be2d184df82c42254669c36b91cdfd2b
Carlos Bruno E <cbruno@spsolutions.com.mx>
2022-01-18 14:37:57 -0600

    Se corrige nombre de variable

--- drift_detection.sh
+++ drift_detection.sh
@@ -45,6 +45,6 @@ if [[ $DRIFTSTATUS = "IN_SYNC" || $DRIFTSTATUS = "DETECTION_FAILED" || $DRIFTSTA
 curl -H 'Content-Type: application/json' \
           --retry 4 \
           --retry-delay 1 \
-          -d '{"text": "'"$DRIFTSTATUS"' - '"$STACKS"' - '"$STACK_NAME"' - '"$MENSAJEDRIFT"'"}' \
+          -d '{"text": "'"$DRIFTSTATUS"' - '"$STACKS"' - '"$STACKNAME"' - '"$MENSAJEDRIFT"'"}' \
           https://mxspsolutions.webhook.office.com/webhookb2/a84b113f-9d27-4138-84ae-e3e4ab4830f2@ac5349df-152e-486f-9b39-fe3c4a25efe0/IncomingWebhook/e2f3db57ac59459a938f44c822548b89/10ed5eda-9b70-4598-858a-e5ae6599fa66; \
           fi
