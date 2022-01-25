539d204c8a328094a04282857c44caf0c7b2d929
Carlos Bruno E <cbruno@spsolutions.com.mx>
2022-01-25 11:44:33 -0600

    ci: Se agrega conversión y se eliminan comentarios innecesarios.

--- switch.py
+++ switch.py
@@ -12,74 +12,11 @@ global args
 
 client = boto3.client('route53')
 
-# Necesitamos el ID de la hosted zone del dominio.
-# Se puede obtener con el comando:
-
-# Primero listamos las zonas desde el AWS CLI con el comando: aws route53 list-hosted-zones
-
-# El ID que nos interesa es el que comienza con Z (ej. Z08055123G7EJOFJG8E8F)
-
-# Esqueleto 
-''' response = client.change_resource_record_sets(
-    HostedZoneId='string',
-    ChangeBatch={
-        'Comment': 'string',
-        'Changes': [
-            {
-                'Action': 'CREATE'|'DELETE'|'UPSERT',
-                'ResourceRecordSet': {    
-                    # Nombre del string [Obligatorio] 
-                    'Name': 'string',
-
-                    # Tipo de redirección [Obligatorio]
-                    'Type': 'SOA'|'A'|'TXT'|'NS'|'CNAME'|'MX'|'NAPTR'|'PTR'|'SRV'|'SPF'|'AAAA'|'CAA'|'DS', 
-
-                    # Routing Policy en caso de que no sea simple, por ejemplo: Weighted, Geolocation, Failover, etc. [Obligatoria]  
-                    'SetIdentifier': 'string',    
-
-                    # Solo aplica cuando el Routing Policy es Weighted. 
-                    'Weight': 123,
-                    
-                    # Solo aplica cuando la Routing Policy es basado en latencia.
-                    'Region': 'us-east-1'|'us-east-2'|'us-west-1'|'us-west-2'|'ca-central-1'|'eu-west-1'|'eu-west-2'|'eu-west-3'|'eu-central-1'|'ap-southeast-1'|'ap-southeast-2'|'ap-southeast-3'|'ap-northeast-1'|'ap-northeast-2'|'ap-northeast-3'|'eu-north-1'|'sa-east-1'|'cn-north-1'|'cn-northwest-1'|'ap-east-1'|'me-south-1'|'ap-south-1'|'af-south-1'|'eu-south-1',
-                    
-                    # Solo aplica cuando la Routing Policy es basado en Geolocalation.
-                    'GeoLocation': {
-                        'ContinentCode': 'string',
-                        'CountryCode': 'string',
-                        'SubdivisionCode': 'string'
-                    },
-
-                    # Solo aplica cuando la Routing Policy es basado es Failover.
-                    'Failover': 'PRIMARY'|'SECONDARY',
-
-                    # Solo aplica cuando la Routing Policy es basado MultiValueAnswer 
-                    'MultiValueAnswer': True|False,
-                    'TTL': 123,
-                    'ResourceRecords': [
-                        {
-                            'Value': 'string'
-                        },
-                    ],
-                    # Esto es requerido cuando se va apuntar a un recurso de AWS. S3, Cloudfront, etc.
-                    'AliasTarget': {
-                        'HostedZoneId': 'string',
-                        'DNSName': 'string',
-                        'EvaluateTargetHealth': True|False
-                    },
-                    'HealthCheckId': 'string',
-                    'TrafficPolicyInstanceId': 'string'
-                }
-            },
-        ]
-    }
-)
-'''
-
 # Failover Principal
-# Se obtienen las variables parseadas
 
+# Se obtienen las variables parseadas
 parser = argparse.ArgumentParser()
+
 # Dominio primario
 parser.add_argument(
     "-pd",
@@ -154,19 +91,12 @@ failover_domain = args.failover_domain
 failover_target = args.failover_target
 
 
-
 # Imprimimos los valores CNAME hacia adonde apunta.
 print("El dominio " + primary_domain + " actualmente apunta a: " + primary_target)
 print("El dominio " + failover_domain + " actualmente apunta a: " + failover_target)
 
-# Conversión de valores de los targets.
-conv_primary_target = primary_target 
-conv_failover_target = failover_target
-
-failover_target = conv_primary_target
-primary_target = conv_failover_target
-
-
+# Conversión de valores de los dominios. Aquí se invierten entre ellos sus valores actuales.
+primary_domain, failover_domain = failover_domain, primary_domain
 
 # Se ejecuta función para switch.
 response = client.change_resource_record_sets(
