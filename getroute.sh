8980b3b5ab1bdfd9741414154edd3398b7b9ad76
Carlos Bruno E <cbruno@spsolutions.com.mx>
2022-01-24 22:36:29 -0600

    ci: Se agregan comillas y doble llave

--- getroute.sh
+++ getroute.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Considerar volverse secretos. Estas son las variables a utilizar en este script.
-HOSTEDZONEID=${env.hosted_zone_id}
-PRIMARY_DOMAIN=${env.primary_domain}
-FAILOVER_DOMAIN=${env.failover_domain}
+HOSTEDZONEID="${{env.hosted_zone_id}}"
+PRIMARY_DOMAIN="${{env.primary_domain}}"
+FAILOVER_DOMAIN="${{env.failover_domain}}"
 
 # Obtener valor de CNAME de produccion.charlycloudy.com
 PRIMARY_TARGET=$(aws route53 list-resource-record-sets --hosted-zone-id ${HOSTEDZONEID} --query "ResourceRecordSets[?Name == '${PRIMARY_DOMAIN}.']" \
