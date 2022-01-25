0e99fcd42571eacc5c2e9580a786f0368c2b3d97
Carlos Bruno E <cbruno@spsolutions.com.mx>
2022-01-25 13:22:28 -0600

    ci: se eliminan paréntesis

--- getroute.sh
+++ getroute.sh
@@ -13,10 +13,10 @@ done
 PRIMARY_TARGET=$(aws route53 list-resource-record-sets --hosted-zone-id ${HOSTEDZONEID} --query "ResourceRecordSets[?Name == '${PRIMARY_DOMAIN}.']" \
 | jq '.[].ResourceRecords[].Value')
 PRIMARY_TARGET=$(echo "${PRIMARY_TARGET//'"'}")
-echo "El dominio " $PRIMARY_DOMAIN " actualmente apunta a: " $PRIMARY_TARGET)
+echo "El dominio " $PRIMARY_DOMAIN " actualmente apunta a: " $PRIMARY_TARGET
 
 # Obtener valor de CNAME Failover
 FAILOVER_TARGET=$(aws route53 list-resource-record-sets --hosted-zone-id ${HOSTEDZONEID} --query "ResourceRecordSets[?Name == '${FAILOVER_DOMAIN}.']" \
 | jq '.[].ResourceRecords[].Value')
 FAILOVER_TARGET=$(echo "${FAILOVER_TARGET//'"'}")
-echo "El dominio " $FAILOVER_DOMAIN " actualmente apunta a: " $FAILOVER_TARGET)
+echo "El dominio " $FAILOVER_DOMAIN " actualmente apunta a: " $FAILOVER_TARGET
