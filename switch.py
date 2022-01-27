4512153ef355d8468cc7324bee467dbc8c7e65a9
Carlos Bruno E <cbruno@spsolutions.com.mx>
2022-01-27 01:33:37 -0600

    R53: Switch funcional

--- switch.py
+++ switch.py
@@ -47,32 +47,25 @@ args = parser.parse_args()
 production_domain = args.production_domain
 hosted_zone_id = args.hosted_zone_id
 
-# Se listan los records actuales de la hosted zone.
 
-response = client.list_resource_record_sets(
-    HostedZoneId=hosted_zone_id
-)
-
-# Aquí generamos la lista desde Route 53:
-listaR53 = response["ResourceRecordSets"]
-
-# Con esto obtenemos el valor del nombre dependiendo del ID de la lista.
-# response["ResourceRecordSets"][0]["Name"]
-
-
-#thislist = {'ResponseMetadata': {'RequestId': '5d8b6033-9bd8-485e-8e0a-023926e2a9b9', 'HTTPStatusCode': 200, 'HTTPHeaders': {'x-amzn-requestid': '5d8b6033-9bd8-485e-8e0a-023926e2a9b9', 'content-type': 'text/xml', 'content-length': '2526', 'date': 'Wed, 26 Jan 2022 17:23:11 GMT'}, 'RetryAttempts': 0}, 'ResourceRecordSets': [{'Name': 'charlycloudy.com.', 'Type': 'A', 'AliasTarget': {'HostedZoneId': 'Z1UJRXOUMOOFQ8', 'DNSName': 'd-3dxauuvmwb.execute-api.us-east-1.amazonaws.com.', 'EvaluateTargetHealth': True}}, {'Name': 'charlycloudy.com.', 'Type': 'NS', 'TTL': 172800, 'ResourceRecords': [{'Value': 'ns-1787.awsdns-31.co.uk.'}, {'Value': 'ns-442.awsdns-55.com.'}, {'Value': 'ns-1186.awsdns-20.org.'}, {'Value': 'ns-714.awsdns-25.net.'}]}, {'Name': 'charlycloudy.com.', 'Type': 'SOA', 'TTL': 900, 'ResourceRecords': [{'Value': 'ns-1787.awsdns-31.co.uk. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400'}]}, {'Name': 'drtarget1.charlycloudy.com.', 'Type': 'CNAME', 'TTL': 300, 'ResourceRecords': [{'Value': 'google.com'}]}, {'Name': 'drtarget2.charlycloudy.com.', 'Type': 'CNAME', 'TTL': 300, 'ResourceRecords': [{'Value': 'stackoverflow.com'}]}, {'Name': 'produccion.charlycloudy.com.', 'Type': 'CNAME', 'SetIdentifier': 'DR region', 'Failover': 'SECONDARY', 'TTL': 60, 'ResourceRecords': [{'Value': 'drtarget1.charlycloudy.com'}], 'HealthCheckId': 'a04de13d-00ea-408a-b0bf-f98aa7d3e948'}, {'Name': 'produccion.charlycloudy.com.', 'Type': 'CNAME', 'SetIdentifier': 'Production region', 'Failover': 'PRIMARY', 'TTL': 60, 'ResourceRecords': [{'Value': 'drtarget2.charlycloudy.com'}], 'HealthCheckId': 'a04de13d-00ea-408a-b0bf-f98aa7d3e948'}, {'Name': 'sps.charlycloudy.com.', 'Type': 'CNAME', 'TTL': 300, 'ResourceRecords': [{'Value': 'spsolutions.com.mx'}]}], 'IsTruncated': False, 'MaxItems': '100'}
-
-# RR = print(thislist["ResourceRecordSets"][5])
-# Contamos la lista de Route 53.
-longRRS = len(listaR53)
-print("En total hay " + str(longRRS) + " registros en Route53 (AWS): \n")
-dominio = "produccion.charlycloudy.com."
-print("Dominio de producción: " + dominio)
-print("#--------------------------------------------------")
 #1 Función para obtener los ResourceRecordSets que contengan como valor produccion.charlycloudy.com
+def obtenerDominios():
 
+    # Se listan los records actuales de la hosted zone.
 
-def obtenerDominios(listaRRS, lengthRRS, dom):
+    response = client.list_resource_record_sets(
+        HostedZoneId=hosted_zone_id
+    )
+
+    # Aquí generamos la lista desde Route 53:
+    listaRRS = response["ResourceRecordSets"]
+
+    # Contamos la lista de Route 53.
+    lengthRRS = len(listaRRS)
+    print("En total hay " + str(lengthRRS) + " registros en Route53 (AWS): \n")
+    dom = "produccion.charlycloudy.com."
+    print("Dominio de producción: " + dom)
+    print("#--------------------------------------------------")
     y = 1
     primario = ""
     secundario = ""
@@ -107,76 +100,76 @@ def obtenerDominios(listaRRS, lengthRRS, dom):
     return primario, secundario
     # print("#" + str(x + 1) + ": " + str(RRSFP))
     # print("El dominio " + dom + " aparece: " + str(y - 1) + " veces")
-# obtenerDominios(listaR53, longRRS, dominio)
+    # obtenerDominios(listaR53, longRRS, dominio)
 
 
-def obtenerFailover():     
-    failovers = obtenerDominios(listaR53, longRRS, dominio)
-    print(failovers[0])
+def obtenerFailover(): 
+    failovers = obtenerDominios()
+    print("Valor actual de primario: " + failovers[0])
+    print("Valor actual de secundario: " + failovers[1])
 
+    primary_domain = failovers[0]
+    failover_domain = failovers[1]
+
+    temp_primary_domain = primary_domain
+    temp_failover_domain = failover_domain
+
+    new_primary_domain = temp_failover_domain
+    new_failover_domain = temp_primary_domain
+
+    print(f"Nuevo primario {new_primary_domain}")
+    print(f"Nuevo failover {new_failover_domain}")
+    return new_primary_domain, new_failover_domain
 obtenerFailover()
-'''
-# Conversión de valores de los dominios. Aquí se invierten entre ellos sus valores actuales.
-temp_primary_domain = primary_domain
-temp_failover_domain = failover_domain
 
-new_primary_domain = temp_failover_domain
-new_failover_domain = temp_primary_domain
-
-print(f"Nuevo primario {new_primary_domain}")
-print(f"Nuevo failover {new_failover_domain}")
-'''
-
-
-# Se comenta Switch hasta obtener los targets PRIMARY y SECONDARY correctamente.
-'''
-records = {
-        'Changes': [
-            {
-                'Action': 'UPSERT',
-                'ResourceRecordSet': {
-                    'Failover': 'PRIMARY',
-                    # Se creo healtcheck para este subdominio
-                    'HealthCheckId': 'a04de13d-00ea-408a-b0bf-f98aa7d3e948',
-                    'Name': production_domain,
-                    'ResourceRecords': [
-                        {
-                            'Value': new_primary_domain,
-                        },
-                    ],
-                    'SetIdentifier': 'Production region',
-                    'TTL': 60,
-                    'Type': 'CNAME',
+# Switch para cambiar failovers.
+def switch():
+    obtenerFailover()
+    failoversNuevos = obtenerFailover()
+    new_primary_domain_switch = failoversNuevos[0]
+    new_secondary_domain_switch = failoversNuevos[1]
+    response = client.change_resource_record_sets(
+    ChangeBatch={
+            'Changes': [
+                {
+                    'Action': 'UPSERT',
+                    'ResourceRecordSet': {
+                        'Failover': 'PRIMARY',
+                        # Se creo healtcheck para este subdominio
+                        'HealthCheckId': 'a04de13d-00ea-408a-b0bf-f98aa7d3e948',
+                        'Name': production_domain,
+                        'ResourceRecords': [
+                            {
+                                'Value': new_primary_domain_switch,
+                            },
+                        ],
+                        'SetIdentifier': 'Production region',
+                        'TTL': 60,
+                        'Type': 'CNAME',
+                    },
                 },
-            },
-            {
-                'Action': 'UPSERT',
-                'ResourceRecordSet': {
-                    'Failover': 'SECONDARY',
-                    # Se usa mismo healtcheck ID para prueba
-                    'HealthCheckId': 'a04de13d-00ea-408a-b0bf-f98aa7d3e948',
-                    'Name': production_domain,
-                    'ResourceRecords': [
-                        {
-                            'Value': new_failover_domain,
-                        },
-                    ],
-                    'SetIdentifier': 'DR region',
-                    'TTL': 60,
-                    'Type': 'CNAME',
+                {
+                    'Action': 'UPSERT',
+                    'ResourceRecordSet': {
+                        'Failover': 'SECONDARY',
+                        # Se usa mismo healtcheck ID para prueba
+                        'HealthCheckId': 'a04de13d-00ea-408a-b0bf-f98aa7d3e948',
+                        'Name': production_domain,
+                        'ResourceRecords': [
+                            {
+                                'Value': new_secondary_domain_switch,
+                            },
+                        ],
+                        'SetIdentifier': 'DR region',
+                        'TTL': 60,
+                        'Type': 'CNAME',
+                    },
                 },
-            },
-        ],
-        'Comment': 'Failover Produccion a DR',
-    }
+            ],
+            'Comment': 'Failover Produccion a DR',
+        },
+        HostedZoneId=hosted_zone_id,
+    )
+switch()
+obtenerDominios()
 
-print(records)
-
-
-response = client.change_resource_record_sets(
-    ChangeBatch=records,
-    HostedZoneId=hosted_zone_id,
-)
-
-print(response)
-'''
