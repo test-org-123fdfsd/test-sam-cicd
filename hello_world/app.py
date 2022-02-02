5612ad3aaed746a8770e920e28f92ed9158bd4e5
Carlos Bruno E <cbruno@spsolutions.com.mx>
2022-02-01 19:35:10 -0600

    LAYERS: Se corrige try

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
-     try:
+    try:
          ip = requests.get("http://checkip.amazonaws.com/")
      except requests.RequestException as e:
          # Send some context about this error to Lambda Logs
