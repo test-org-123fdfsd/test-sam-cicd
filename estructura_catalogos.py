c04dbd905f9c53e96b1cf6dd3d12d6c873f6b4e1
Carlos Bruno E <cbruno@spsolutions.com.mx>
2022-03-04 14:21:19 -0600

    DYNAMO: Se obtiene lista de tablas que no están en ESTRUCTURA CATALOGOS

--- estructura_catalogos.py
+++ estructura_catalogos.py
@@ -1,26 +1,22 @@
+from ast import NotIn
 import os
 from re import X
+from unittest import result
 import boto3
 ### El profile_name se debe eliminar previo a la implementación en workflow.
 session = boto3.Session(profile_name='principal-dev', region_name='us-east-1')
 dynamo = session.client('dynamodb')
 
 
-#validar_tablas(tablasListaEnv)
+#Se genera lista de tablas
 response = dynamo.list_tables()
 
-
-
 #response = dynamo.describe_table(TableName='sia-gen-adm-estructura-catalogos-dev')
-listTablas = response['TableNames']
+listTablasSIA = response['TableNames']
+#Se le quita el ambiente a las tablas que la tengan para validar items
+listaTablasItems = [w.replace('-dev', '') for w in listTablasSIA]
 
-#get_items(listTablas)
-
-#from boto3.dynamodb.conditions import Key, Attr
-
-#dynamodb = boto3.resource('dynamodb')
-
-
+##### Se genera lista de items de tabla de 
 listaItems = []
 def obtener_items():
     result = None
@@ -30,12 +26,29 @@ def obtener_items():
             response = dynamo.scan(
             TableName='sia-gen-adm-estructura-catalogos-dev')
             itemTabla = response['Items'][x]['NOMBRE']['S']
-            print(itemTabla)
             x = x + 1
             listaItems.append(itemTabla)
         except:
             result = 'OK'
             print("Se terminó de generar la lista de items")
 obtener_items()
-print(len(listaItems))
 
+print(f'Total de items en lista: {len(listaItems)}')
+
+tablasNoEstructura = []
+def validador_items():
+    x = 0
+    result = None
+    while result is None:
+        try:
+            if listaTablasItems[x] not in listaItems:
+                tablasNoEstructura.append(listaTablasItems[x])
+                print(f'{listaTablasItems[x]} no está en la tabla de Estructura Catalogos')
+            x = x + 1
+        except:
+            result = 'OK'
+            print("Proceso terminado")    
+validador_items()
+print("Lista de tablas NO ESTÁN EN ESCRUCTURA:")
+print(tablasNoEstructura)
+print(f'Total de tablas que no están en Estructura Catálogos: {len(tablasNoEstructura)}')
