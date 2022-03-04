d99d2ab6ebbfb56780ad65efa6a7b4d69f10eed7
Carlos Bruno E <cbruno@spsolutions.com.mx>
2022-03-03 19:41:53 -0600

    DYNAMO: Se regresa a get_item

--- estructura_catalogos.py
+++ estructura_catalogos.py
@@ -1,6 +1,5 @@
 import os
 import boto3
-import time
 ### El profile_name se debe eliminar previo a la implementación en workflow.
 session = boto3.Session(profile_name='principal-dev', region_name='us-east-1')
 dynamo = session.client('dynamodb')
@@ -13,44 +12,22 @@ listTablas = response['TableNames']
 
 #response = dynamo.describe_table(TableName='sia-gen-adm-estructura-catalogos-dev')
 
-#get_items(listTablas)
-listaTablasExist = []
-listaTablasInexis = []
 
 def get_items(tablas):
     for x in tablas:
-        #time.sleep(3)
         try:
-            
-            response = dynamo.batch_get_item(
-                RequestItems={
-                    'sia-gen-adm-estructura-catalogos-dev': {
-                        'Keys': [
-                            {
-                                'NOMBRE': {
-                                    'S': 'sia-afore-cat-tipo-valor-dev'
-                                }
-                            },
-                        ]
-                        }
-                    }
+            response = dynamo.get_item(
+            TableName='sia-gen-adm-estructura-catalogos-dev',
+            Key={
+                'NOMBRE': {
+                    'S': x
+                }
+            }
             )
-            #print(response)
-            print(x)
-            print(response['Responses']['sia-gen-adm-estructura-catalogos-dev'][0]['NOMBRE']['S'])
-            #time.sleep(3)
-            listaTablasExist.append(x)
-        except Exception as e:
-            print(e)
-            listaTablasInexis.append(x)
-            #print(f'{x} no está en la lista')
-
-#if listaTablasExist != []:
+            if response != 0:
+                print(f'{x} no está en la tabla de Estructura de Catálogos')
+        except:
+            print(f'{x} no está en la tabla de Estructura de Catálogos')
 
 
 get_items(listTablas)
-print('Tablas existentes: ' + ', '.join(listaTablasExist))
-
-#if listaTablasInexis != []:
-print('Tablas inexistentes: ' + ', '.join(listaTablasInexis))
-print(len(listaTablasInexis))
